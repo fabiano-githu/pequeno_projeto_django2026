0e6805ac9b8af839a3daeafc91de12e6f089d16c
chavierwebmidia2026-glitch <chavierwebmidia2026@gmail.com>
2026-08-17 22:50:21 -0300

    Projeto Django inicial

---
+++ page_static/views.py
@@ -0,0 +1,15 @@
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'page_static/home.html')
+
+
+def contatos(request):
+    return render(request, 'page_static/contatos.html')
+
+
+def sobre(request):
+    return render(request, 'page_static/sobre.html')
